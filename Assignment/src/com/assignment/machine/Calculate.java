93e6043402f9dff5fc20591a3e481ef4fa51a4fd
Eoin Gallagher <c17400202@mydit.ie>
2019-04-11 12:53:13 +0100

    changes to calculate.java

--- Assignment/src/com/assignment/machine/Calculate.java
+++ Assignment/src/com/assignment/machine/Calculate.java
@@ -6,22 +6,115 @@ public class Calculate
 {
 	ArrayList<Patient> pList = new ArrayList<Patient>();
 	
+	private float tempYesTotal;
+	private float tempNoTotal;
+	private float acheYesTotal;
+	private float acheNoTotal;
+	private float soreTYesTotal;
+	private float soreTNoTotal;
+	private float tsYesTotal;
+	private float tsNoTotal;
+	private float dataTotal;
+	
 	private float tempYesTS;
 	private float tempNoTS;
 	private float achesYesTS;
 	private float achesNoTS;
 	private float soreTYesTS;
 	private float soreTNoTS;
+	private float yesTS;
+	private float noTS;
+	
+	private float probYes;
+	private float probNo;
+	private float probTotal;
 	private float chanceYes;
 	private float chanceNo;
 	
+	int i;
+	
+	Patient currentP;
 	private String currentTemp;
 	private String currentAches;
 	private String currentSoreT;
 	
+	//Constructor
 	public Calculate()
 	{
+		FileProcess fp = new FileProcess("src\\com\\assignment\\machine\\dataset.csv");
+		fp.openFile();
+		pList = fp.readFile();
+		fp.closefile();
+		System.out.println(pList);
+	}
+	public void Total()
+	{
+		for(i = 0; i < pList.size(); i++)
+		{
+			if()
+			{
+				dataTotal++;
+			}
+			if()
+			{
+				tempYesTotal++;
+			}
+			else
+			{
+				tempNoTotal++;
+			}
+			if()
+			{
+				tsYesTotal++;
+			}
+			else
+			{
+				tsNoTotal++;
+			}
+			if()
+			{
+				acheYesTotal++;
+			}
+			else
+			{
+				acheNoTotal++;
+			}
+			if()
+			{
+				soreTYesTotal++;
+			}
+			else
+			{
+				soreTNoTotal++;
+			}
+			if()
+			{
+				tsYesTotal++;
+			}
+			else
+			{
+				tsNoTotal++;
+			}
+			
+			
+		}
+		tempYesTS = (tempYesTotal/tsYesTotal);
+		tempNoTS = (tempNoTotal/tsNoTotal);
+		achesYesTS = (acheYesTotal/tsYesTotal);
+		achesNoTS = (acheNoTotal/tsNoTotal);
+		soreTYesTS = (soreTYesTotal/tsYesTotal);
+		soreTNoTS = (soreTNoTotal/tsNoTotal);
+		yesTS = (tsYesTotal/dataTotal);
+		noTS = (tsNoTotal/dataTotal);
 		
+	}
+	public void Algorithm()
+	{
+		probYes = (tempYesTS * achesYesTS * soreTYesTS * yesTS);
+		probNo = (tempNoTS * achesNoTS * soreTNoTS * noTS);
+		probTotal = (probYes + probNo);
+		chanceYes = (probYes/probTotal);
+		chanceNo = (probNo/probTotal);
 	}
 
 	public float getTempYesTS() {
@@ -110,6 +203,94 @@ public class Calculate
 
 	public void setCurrentSoreT(String currentSoreT) {
 		this.currentSoreT = currentSoreT;
+	}
+
+	public float getYesTS() {
+		return yesTS;
+	}
+
+	public void setYesTS(float yesTS) {
+		this.yesTS = yesTS;
+	}
+
+	public float getNoTS() {
+		return noTS;
+	}
+
+	public void setNoTS(float noTS) {
+		this.noTS = noTS;
+	}
+	private float getProbYes() {
+		return probYes;
+	}
+	private void setProbYes(float probYes) {
+		this.probYes = probYes;
+	}
+	public float getProbNo() {
+		return probNo;
+	}
+	public void setProbNo(float probNo) {
+		this.probNo = probNo;
+	}
+	public float getProbTotal() {
+		return probTotal;
+	}
+	public void setProbTotal(float probTotal) {
+		this.probTotal = probTotal;
+	}
+	public float getTempYesTotal() {
+		return tempYesTotal;
+	}
+	public void setTempYesTotal(float tempYesTotal) {
+		this.tempYesTotal = tempYesTotal;
+	}
+	public float getTempNoTotal() {
+		return tempNoTotal;
+	}
+	public void setTempNoTotal(float tempNoTotal) {
+		this.tempNoTotal = tempNoTotal;
+	}
+	public float getAcheYesTotal() {
+		return acheYesTotal;
+	}
+	public void setAcheYesTotal(float acheYesTotal) {
+		this.acheYesTotal = acheYesTotal;
+	}
+	public float getAcheNoTotal() {
+		return acheNoTotal;
+	}
+	public void setAcheNoTotal(float acheNoTotal) {
+		this.acheNoTotal = acheNoTotal;
+	}
+	public float getSoreTYesTotal() {
+		return soreTYesTotal;
+	}
+	public void setSoreTYesTotal(float soreTYesTotal) {
+		this.soreTYesTotal = soreTYesTotal;
+	}
+	public float getSoreTNoTotal() {
+		return SoreTNoTotal;
+	}
+	public void setSoreTNoTotal(float soreTNoTotal) {
+		SoreTNoTotal = soreTNoTotal;
+	}
+	public float getTsYesTotal() {
+		return tsYesTotal;
+	}
+	public void setTsYesTotal(float tsYesTotal) {
+		this.tsYesTotal = tsYesTotal;
+	}
+	public float getTsNoTotal() {
+		return tsNoTotal;
+	}
+	public void setTsNoTotal(float tsNoTotal) {
+		this.tsNoTotal = tsNoTotal;
+	}
+	public float getDataTotal() {
+		return dataTotal;
+	}
+	public void setDataTotal(float dataTotal) {
+		this.dataTotal = dataTotal;
 	}
 
 }
